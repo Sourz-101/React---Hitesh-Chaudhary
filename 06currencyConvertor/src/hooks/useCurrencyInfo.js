bf67e9810eb2222364d99131b90ce18ac7f17169
Sourabh <sourabh4pvt@gmail.com>
2024-02-07 08:46:07 +0530

    currency convertor completed

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -6,11 +6,10 @@ function useCurrencyInfo(currency){
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-        console.log(data);
+        .then((res) => setData(res[currency]));
     }, [currency])
     console.log(data);
-    return data
+    return data;
 }
 
 export default useCurrencyInfo;
